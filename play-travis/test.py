66f9df91595bbfc5b28c1c7026deff0101c2875f
timofey-ilin <tilin@diasoft.ru>
2021-03-17 17:25:18 +0300

    change 0 test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -1,6 +1,6 @@
 import unittest
 class NumbersTest(unittest.TestCase):
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 0, 1)
 if __name__ == '__main__':
     unittest.main()
